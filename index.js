8c3bda3bbae612f86a838f1b22be89ab42d0b4d8
Noel Manuel <setsuna04@gmail.com>
2018-02-07 21:36:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ const app = "I don't do much."
 kittens = [];
 
 function destructivelyAppendKitten(name) {
-    kittens.push(name)
+  kittens.push(name)
 }
 
 function destructivelyPrependKitten(name) {
   kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.unshift();
 }
